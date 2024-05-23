c2ab60aaed607571fda9d996abae6b0adb9517ff
carlo <carlomonti83@gmail.com>
2024-05-23 14:48:35 +0200

    fix issues

--- plot_functions.py
+++ plot_functions.py
@@ -3,13 +3,91 @@ import numpy as np
 import math
 from sklearn.preprocessing import StandardScaler
 
-def plot_fhr_trace(fhr,ground_truth_fhr,fhr_trace_er_length,fs,results,width=15):
+def plot_signal(signal_combo,fs,section_n=0,sections=1,width=15,signal_type="ECG signal",signals_labels=None,different_sig=None,row_height=1.5):
+
   '''
-  This function takes in a signal and plot only a part of it. It also plots the
-  ECG annotations.
+  This function takes in a signal and plot only a part of it.
   E.g.
   If sections=5 and section_n=2 this function will divide the signal
   into 5 sections and prints only the second one
+  '''
+  if signals_labels is None: 
+    signals_labels = []
+  if different_sig is None: 
+    different_sig = []
+  signal = signal_combo.copy()
+
+  if section_n >= sections:
+      print("Error: section_n > sections")
+      return
+  
+  if signal.ndim > 1:
+    if signal.shape[0] == 1:
+        signal = signal[0]
+        rows = 1
+    else:
+        rows = signal.shape[0]
+  else:
+    rows = 1
+
+  plt.rcParams['figure.figsize'] = [width, 1.5]
+  plt.rcParams['lines.linewidth'] = 0.5
+
+  f, ax = plt.subplots(rows, sharex=True)
+  f.set_figwidth(width)
+  f.set_figheight(row_height*rows)
+
+  l = []
+  l_name = []
+
+  if len(signals_labels) == 0:
+    for i in range(0,rows):
+      signals_labels.append("Signal " + str(i+1))
+          
+  is_first = True
+  for i in range(0,rows):
+    if rows > 1:
+        sig = signal[i]
+    else:
+        sig = signal
+    section_length = round(len(sig)/sections)
+    start = section_n * section_length
+    end = (start+section_length) -1
+    if end > len(sig)-1:
+        end = len(sig)-1
+    xaxis = np.array(range(start,end))
+    xaxis = xaxis/fs
+    if rows > 1:
+      ax[i].set_ylabel(signals_labels[i])
+    else:
+      ax.set_ylabel(signals_labels[i])
+    if len(different_sig) > 0 and i in different_sig[0]:
+      l1, = ax[i].plot(xaxis,sig[start:end],color=different_sig[1])
+      ax[i].set_ylabel(different_sig[3])
+      l.append(l1)
+      l_name.append(different_sig[2])
+    else:
+      if rows > 1:
+        l1, = ax[i].plot(xaxis,sig[start:end])
+      else:
+        l1, = ax.plot(xaxis,sig[start:end])
+      if is_first:
+        l_name.append(signal_type)
+        l.append(l1)
+        is_first = False
+
+  f.tight_layout()
+  plt.xlabel("Time (s)")
+  plt.legend(l,l_name,loc='upper center', bbox_to_anchor=(0.5, -0.4),fancybox=True, ncol=5)
+  plt.show()
+  return
+
+def plot_fhr_trace(fhr,ground_truth_fhr,fhr_trace_er_length,fs,results,width=15):
+  '''
+  This function takes in two fhr traces, plot them and underlines the sections in which the difference
+  is beyond the max value (given a proper signal is provided).
+  Input: the fhr trace, the real fhr trace, the error signal
+  Output: a plot of the two fhr trace + a colored area thar represent the sections where the difference of the two is above threshold
   '''
   plt.rcParams['figure.figsize'] = [width, 1.5]
   plt.rcParams['lines.linewidth'] = 0.5
@@ -114,11 +192,11 @@ def plot_error_timeline(signal,annotations,fs,results,ground_truth,section_n=0,s
 
 def plot_signal_annotations(signal,annotations,fs,section_n=0,sections=1,ground_truth=[],annotation_symbol=[],ground_truth_symbol=[],signal_label="ECG signal",width=15):
   '''
-  This function takes in a signal and plot only a part of it. It also plots the
-  ECG annotations.
-  E.g.
-  If sections=5 and section_n=2 this function will divide the signal
-  into 5 sections and prints only the second one
+  This function takes in a signal and plot it with the ECG annotations and/or the detected beats.
+  This function can also slice the signal and plot only a single slice.
+  E.g. If sections=5 and section_n=2 this function will divide the signal into 5 sections and prints only the second one
+  Input: the ecg signal, the annotations/detected beats, the sampling frequency, the section n, the number of sections, the ground truth signal
+  Outpu: a plot of the ecg and lines (or dots) for the annotations/detected beats and the ground truth
   '''
 
   if section_n >= sections:
@@ -195,95 +273,7 @@ def plot_signal_annotations(signal,annotations,fs,section_n=0,sections=1,ground_
 
   
   plt.xlabel("Time (s)")
-  plt.ylabel("Amplitude (uV)")
   plt.plot(xaxis,signal[start:end],label=signal_label)
   plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.4),fancybox=True, ncol=5)
-  plt.show()
-  return
-
-def plot_signal(signal_combo,fs,section_n=0,sections=1,width=15,signal_type="ECG signal",signals_labels=None,different_sig=None,row_height=1.5):
-
-  '''
-  This function takes in a signal and plot only a part of it.
-  E.g.
-  If sections=5 and section_n=2 this function will divide the signal
-  into 5 sections and prints only the second one
-
-  signal_labels are labels for the y axis
-  signal_type is the type printed in the legend
-  different_signal is for the signal to be printed different:
-  -index
-  -color
-  -type of signal for the legend
-  -label for y axis
-  '''
-  if signals_labels is None: 
-    signals_labels = []
-  if different_sig is None: 
-    different_sig = []
-  signal = signal_combo.copy()
-
-  if section_n >= sections:
-      print("Error: section_n > sections")
-      return
-  
-  if signal.ndim > 1:
-    if signal.shape[0] == 1:
-        signal = signal[0]
-        rows = 1
-    else:
-        rows = signal.shape[0]
-  else:
-    rows = 1
-
-  plt.rcParams['figure.figsize'] = [width, 1.5]
-  plt.rcParams['lines.linewidth'] = 0.5
-
-  f, ax = plt.subplots(rows, sharex=True)
-  f.set_figwidth(width)
-  f.set_figheight(row_height*rows)
-
-  l = []
-  l_name = []
-
-  if len(signals_labels) == 0:
-    for i in range(0,rows):
-      signals_labels.append("Signal " + str(i+1))
-          
-  is_first = True
-  for i in range(0,rows):
-    if rows > 1:
-        sig = signal[i]
-    else:
-        sig = signal
-    section_length = round(len(sig)/sections)
-    start = section_n * section_length
-    end = (start+section_length) -1
-    if end > len(sig)-1:
-        end = len(sig)-1
-    xaxis = np.array(range(start,end))
-    xaxis = xaxis/fs
-    if rows > 1:
-      ax[i].set_ylabel(signals_labels[i])
-    else:
-      ax.set_ylabel(signals_labels[i])
-    if len(different_sig) > 0 and i in different_sig[0]:
-      l1, = ax[i].plot(xaxis,sig[start:end],color=different_sig[1])
-      ax[i].set_ylabel(different_sig[3])
-      l.append(l1)
-      l_name.append(different_sig[2])
-    else:
-      if rows > 1:
-        l1, = ax[i].plot(xaxis,sig[start:end])
-      else:
-        l1, = ax.plot(xaxis,sig[start:end])
-      if is_first:
-        l_name.append(signal_type)
-        l.append(l1)
-        is_first = False
-
-  f.tight_layout()
-  plt.xlabel("Time (s)")
-  plt.legend(l,l_name,loc='upper center', bbox_to_anchor=(0.5, -0.4),fancybox=True, ncol=5)
   plt.show()
   return
